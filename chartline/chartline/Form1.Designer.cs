203cdf0f3a7c258071970eacf0cd72011344d7d6
krmnkrn <karamankarani42@gmail.com>
2020-09-18 03:28:41 +0300

    Line graph added

--- chartline/chartline/Form1.Designer.cs
+++ chartline/chartline/Form1.Designer.cs
@@ -1,5 +1,7 @@
 ﻿namespace chartline
 {
+
+
     partial class Form1
     {
         /// <summary>
@@ -29,12 +31,203 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.btnGonder = new System.Windows.Forms.Button();
+            this.btnBaglan = new System.Windows.Forms.Button();
+            this.btnKes = new System.Windows.Forms.Button();
+            this.btnEkraniTemizle = new System.Windows.Forms.Button();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnAc = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnGonder
+            // 
+            this.btnGonder.Location = new System.Drawing.Point(0, 0);
+            this.btnGonder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGonder.Name = "btnGonder";
+            this.btnGonder.Size = new System.Drawing.Size(100, 28);
+            this.btnGonder.TabIndex = 39;
+            // 
+            // btnBaglan
+            // 
+            this.btnBaglan.Location = new System.Drawing.Point(0, 0);
+            this.btnBaglan.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBaglan.Name = "btnBaglan";
+            this.btnBaglan.Size = new System.Drawing.Size(100, 28);
+            this.btnBaglan.TabIndex = 38;
+            // 
+            // btnKes
+            // 
+            this.btnKes.Location = new System.Drawing.Point(0, 0);
+            this.btnKes.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKes.Name = "btnKes";
+            this.btnKes.Size = new System.Drawing.Size(100, 28);
+            this.btnKes.TabIndex = 37;
+            // 
+            // btnEkraniTemizle
+            // 
+            this.btnEkraniTemizle.Location = new System.Drawing.Point(0, 0);
+            this.btnEkraniTemizle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEkraniTemizle.Name = "btnEkraniTemizle";
+            this.btnEkraniTemizle.Size = new System.Drawing.Size(100, 28);
+            this.btnEkraniTemizle.TabIndex = 36;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(0, 0);
+            this.btnKaydet.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(100, 28);
+            this.btnKaydet.TabIndex = 35;
+            // 
+            // btnAc
+            // 
+            this.btnAc.Location = new System.Drawing.Point(0, 0);
+            this.btnAc.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnAc.Name = "btnAc";
+            this.btnAc.Size = new System.Drawing.Size(100, 28);
+            this.btnAc.TabIndex = 34;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tabControl1.ItemSize = new System.Drawing.Size(600, 40);
+            this.tabControl1.Location = new System.Drawing.Point(0, -39);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.RightToLeftLayout = true;
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1392, 749);
+            this.tabControl1.TabIndex = 17;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.zedGraphControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 44);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tabPage1.Size = new System.Drawing.Size(1384, 701);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Summary";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(370, 9);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(9, 9, 9, 9);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(1005, 514);
+            this.zedGraphControl1.TabIndex = 32;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(385, 757);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 39);
+            this.label9.TabIndex = 29;
+            this.label9.Text = "Time:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label10.Location = new System.Drawing.Point(553, 757);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(37, 39);
+            this.label10.TabIndex = 30;
+            this.label10.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 551);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(218, 35);
+            this.button1.TabIndex = 33;
+            this.button1.Text = "Start Graph";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(222, 617);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(218, 35);
+            this.button2.TabIndex = 34;
+            this.button2.Text = "End Graph";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1409, 858);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.btnAc);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.btnEkraniTemizle);
+            this.Controls.Add(this.btnKes);
+            this.Controls.Add(this.btnBaglan);
+            this.Controls.Add(this.btnGonder);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Real Time Graph";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Button btnGonder;
+        private System.Windows.Forms.Button btnBaglan;
+        private System.Windows.Forms.Button btnKes;
+        private System.Windows.Forms.Button btnEkraniTemizle;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnAc;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- chartline/chartline/Form1.cs
+++ chartline/chartline/Form1.cs
@@ -5,16 +5,89 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Diagnostics;
+using ZedGraph;
 
 namespace chartline
 {
     public partial class Form1 : Form
     {
+        GraphPane myPane = new GraphPane();
+        RollingPointPairList listPointsOne = new RollingPointPairList(40);
+        LineItem myCurveOne;
+        RollingPointPairList listPointsTwo = new RollingPointPairList(40);
+        LineItem myCurveTwo;
+        RollingPointPairList listPointsThree = new RollingPointPairList(40);
+        LineItem myCurveThree;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //1
+            myPane = zedGraphControl1.GraphPane;
+            myPane.XAxis.Title.Text = "Time";
+            myPane.YAxis.Title.Text = "Temprature";
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Refresh();
+
+        }
+        /// <summary>
+        /// aklımdaki zamanı verecem 
+        /// ona göre  gelen sıcak lık da olursa ekrana basacak 
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            int sayi = 0;
+            sayi++;
+            label10.Text = sayi.ToString();
+            //
+            myPane = zedGraphControl1.GraphPane;
+            //1
+            double x = Convert.ToDouble(label10.Text);
+            double y1 = double.Parse("0", System.Globalization.CultureInfo.InvariantCulture);
+            listPointsOne.Add(x, y1);
+            myCurveOne = myPane.AddCurve(null, listPointsOne, Color.Blue, SymbolType.Circle);
+            
+            //2            
+            double y2 = double.Parse("0", System.Globalization.CultureInfo.InvariantCulture);
+            listPointsOne.Add(x, y2);
+            myCurveTwo = myPane.AddCurve(null, listPointsTwo, Color.Red, SymbolType.Star);
+            
+            //3
+            double y3 = double.Parse("0", System.Globalization.CultureInfo.InvariantCulture);
+            listPointsOne.Add(x, y3);
+            myCurveThree = myPane.AddCurve(null, listPointsThree, Color.Green, SymbolType.Triangle);
+            
+            //Grafiği yenileme
+            zedGraphControl1.Invalidate();
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Refresh();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled= true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
         }
     }
 }
